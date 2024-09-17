cd8c796a92e9c64721c1056b180d5255434b16cb
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2024-09-17 13:49:15 -0400

    adding flutter support

--- lua/plugins/flutter-lsp.lua
+++ lua/plugins/flutter-lsp.lua
@@ -1,12 +1,28 @@
 local flutter_path = "/Users/joncarlostavarez/fvm"
-
+local keymap = vim.keymap.set
 return {
   -- for DAP support
-  { "mfussenegger/nvim-dap" },
+  {
+    "mfussenegger/nvim-dap",
+
+    debugger = {
+      enabled = true,
+      register_configurations = function()
+        require("dap").configurations.dart = {}
+        require("dap.ext.vscode").load_launchjs()
+      end,
+    },
+  },
   {
     "akinsho/flutter-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "stevearc/dressing.nvim" },
     config = function()
+      keymap("n", "<leader>FS", ":FlutterRun <CR>", {})
+      keymap("n", "<leader>FQ", ":FlutterQuit <CR>", {})
+      keymap("n", "<leader>FR", ":FlutterRestart <CR>", {})
+      keymap("n", "<leader>LR", ":FlutterLspRestart <CR>", {})
+      keymap("n", "<leader>FO", ":FlutterOpen <CR>", {})
+      keymap("n", "<leader>FD", ":FlutterDevTools <CR>", {})
       require("flutter-tools").setup({
         -- (uncomment below line for windows only)
         -- flutter_path = "home/flutter/bin/flutter.bat",
@@ -39,27 +55,33 @@ return {
         },
         dev_log = {
           -- toggle it when you run without DAP
-          enabled = false,
+          enabled = true,
           open_cmd = "tabedit",
+        },
+        dev_tools = {
+          autostart = true,
+          auto_open_browser = false,
+        },
+        widget_guides = {
+          enabled = true,
         },
         lsp = {
           color = { -- show the derived colours for dart variables
-            enabled = false, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
-            background = false, -- highlight the background
+            enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+            background = true, -- highlight the background
             background_color = nil, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
             foreground = false, -- highlight the foreground
             virtual_text = true, -- show the highlight using virtual text
             virtual_text_str = "■", -- the virtual text character to highlight
           },
-          {
+          settings = {
             showTodos = true,
             completeFunctionCalls = true,
             analysisExcludedFolders = { flutter_path },
             renameFilesWithClasses = "prompt", -- "always"
             enableSnippets = true,
             updateImportsOnRename = true, -- Whether to update imports and other directives when files are renamed. Required for `FlutterRename` command.
-          }, -- on_attach = require("lspconfig").common_on_attach,
-          -- capabilities = require("lspconfig").default_capabilities,
+          },
         },
       })
     end,
