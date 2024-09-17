a7c080279cde5943061d25b166a775da88dc3b57
Joncarlos Tavarez <joncarlos.tavarez@gmail.com>
2024-09-17 12:16:28 -0400

    adding lazy vim

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,15 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local keymap = vim.keymap.set
+
+-- Normal Mode
+keymap("n", "<S-j>", "10j", { desc = "Jump 10 Lines Down" })
+keymap("n", "<S-k>", "10k", { desc = "Jump 10 Lines Up" })
+
+-- Insert Mode
+keymap("i", "jk", "<ESC>", { desc = "Exit Insert Mode" })
+keymap("i", "<C-h>", "<Left>", { desc = "Move Cursor Left" })
+keymap("i", "<C-l>", "<Right>", { desc = "Move Cursor Right" })
+keymap("i", "jk", "<ESC>", { desc = "Exit Insert Mode" })
---
+++ lua/plugins/flutter-lsp.lua
@@ -0,0 +1,71 @@
+local flutter_path = "/Users/joncarlostavarez/fvm"
+
+return {
+  -- for DAP support
+  { "mfussenegger/nvim-dap" },
+  {
+    "akinsho/flutter-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "stevearc/dressing.nvim" },
+    config = function()
+      require("flutter-tools").setup({
+        -- (uncomment below line for windows only)
+        -- flutter_path = "home/flutter/bin/flutter.bat",
+
+        debugger = {
+          -- make these two params true to enable debug mode
+          enabled = false,
+          run_via_dap = false,
+          register_configurations = function(_)
+            require("dap").adapters.dart = {
+              type = "executable",
+              command = vim.fn.stdpath("data") .. "/mason/bin/dart-debug-adapter",
+              args = { "flutter" },
+            }
+
+            require("dap").configurations.dart = {
+              {
+                type = "dart",
+                request = "launch",
+                name = "Launch flutter",
+                dartSdkPath = flutter_path,
+                flutterSdkPath = flutter_path,
+                -- program = "${workspaceFolder}/lib/main.dart",
+                -- cwd = "${workspaceFolder}",
+              },
+            }
+            -- uncomment below line if you've launch.json file already in your vscode setup
+            -- require("dap.ext.vscode").load_launchjs()
+          end,
+        },
+        dev_log = {
+          -- toggle it when you run without DAP
+          enabled = false,
+          open_cmd = "tabedit",
+        },
+        lsp = {
+          color = { -- show the derived colours for dart variables
+            enabled = false, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+            background = false, -- highlight the background
+            background_color = nil, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
+            foreground = false, -- highlight the foreground
+            virtual_text = true, -- show the highlight using virtual text
+            virtual_text_str = "■", -- the virtual text character to highlight
+          },
+          {
+            showTodos = true,
+            completeFunctionCalls = true,
+            analysisExcludedFolders = { flutter_path },
+            renameFilesWithClasses = "prompt", -- "always"
+            enableSnippets = true,
+            updateImportsOnRename = true, -- Whether to update imports and other directives when files are renamed. Required for `FlutterRename` command.
+          }, -- on_attach = require("lspconfig").common_on_attach,
+          -- capabilities = require("lspconfig").default_capabilities,
+        },
+      })
+    end,
+  },
+  -- for dart syntax hightling
+  {
+    "dart-lang/dart-vim-plugin",
+  },
+}
